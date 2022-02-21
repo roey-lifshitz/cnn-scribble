e4d4684123eb0ae376e87eb037feb92ba2817eea
roey-lifshitz <45150099+roey-lifshitz@users.noreply.github.com>
2022-02-21 20:10:27 +0200

    Focus Bar on input box

--- Ui/input_box.py
+++ Ui/input_box.py
@@ -12,6 +12,8 @@ class InputBox:
                  text_color= (0, 0, 0),
                  border_color=(0, 0, 0),
                  border_width=2,
+                 focus_offset=9,
+                 focus_interval=0.5
                  ):
 
         self.padding = padding
@@ -19,11 +21,12 @@ class InputBox:
         self.text_color = text_color
         self.border_color = border_color
         self.border_width = border_width
-        self.focus_color = (0, 0, 0)
-
+        self.focus_color = (255, 255, 255)
+        self.focus_offset = focus_offset
+        self.focus_interval = focus_interval
         self.font = pygame.font.SysFont(font, font_size)
-        self.clock = None
-        self.time = -1
+        self.clock = pygame.time.Clock()
+        self.time = 0
         self.hover = False
 
         # Get the rect of the button
@@ -36,13 +39,12 @@ class InputBox:
     def _finish(self):
         print(self.text)
 
-    def _draw_focus(self):
-        # Swap color between white and black evry 2 seconds
-        print(self.time)
-        if self.time % 2000 == 0:
-            self.time = -1
-            self.focus_color = tuple(c ^ 255 for c in self.focus_color)
-            print(self.focus_color)
+    def _draw_focus(self, screen, color):
+        # Calculate focus location
+        x = self.x + self.font.render(self.text[:self.index], True, self.text_color).get_rect().width
+        x += self.focus_offset
+
+        pygame.draw.line(screen, color, (x, self.y * 1.01), (x, self.y * 0.99 + self.h), 2)
 
     def draw(self, screen):
 
@@ -63,23 +65,31 @@ class InputBox:
         data_rect = data.get_rect()
         data_rect.midleft = self.rect.midleft[0] + self.padding, self.rect.midleft[1]
 
+        if self.hover:
+            self.time += self.clock.tick()
+
+        if self.time > self.focus_interval * 1000:
+            self.focus_color = tuple(c ^ 255 for c in self.focus_color)
+            self.time = 0
+
+        self._draw_focus(screen, self.focus_color)
+
         screen.blit(data, data_rect)
-        self._draw_focus()
+
+
+
 
     def update(self, event):
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             if self.rect.collidepoint(*pygame.mouse.get_pos()):
                 self.hover = True
-                self.clock = pygame.time.Clock()
             # Continue writing until clicked again
             else:
                 self.hover = False
-                self.clock = None
-                self.time = -1
+                self.focus_color = (255, 255, 255)
 
         if self.hover:
-            self.time += self.clock.tick()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
                     self._finish()
@@ -89,12 +99,10 @@ class InputBox:
                 elif event.key == pygame.K_LEFT:
                     self.index = max(0, self.index - 1)
                 elif event.key == pygame.K_RIGHT:
-                    self.index = min(len(self.text), len(self.text) + 1)
+                    self.index = min(len(self.text), self.index + 1)
                 else:
                     # Check if can add a new letter
                     new_width = self.font.render(self.text + event.unicode, True, self.text_color).get_rect()
                     if new_width.w < self.w - self.padding * 2:
                         self.index += 1
-                        self.text += event.unicode
-
-
+                        self.text = self.text[:self.index - 1] + event.unicode + self.text[self.index - 1:]
