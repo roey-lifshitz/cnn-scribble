1fddb0e7300b579c6c57ab301653787cb05c4a9a
roey-lifshitz <45150099+roey-lifshitz@users.noreply.github.com>
2022-05-23 00:11:32 +0300

    refactoring

--- trainer.py
+++ trainer.py
@@ -1,13 +1,6 @@
-import pygame
-import numpy as np
-import threading
-import time
-from UI.canvas import Canvas
-from UI.button import Button
-from UI.input_box import InputBox
-from UI.timer import Timer
+
 from file_parser import FileParser
-from NeuralNetwork.neural_network import NeuralNetwork
+from NeuralNetwork.model import Model
 from NeuralNetwork.layers import Convolutional, Pooling, Flatten, Dense, Dropout
 from NeuralNetwork.activations import Relu, Softmax, Sigmoid
 from NeuralNetwork.losses import CrossEntropyLoss
@@ -17,23 +10,26 @@ from NeuralNetwork.optimizers import Adam
 def main():
  
     file_parser = FileParser()
-    train_x, train_y, test_x, test_y = file_parser.load(train_amount=1000, test_amount=100)
+    train_x, train_y, test_x, test_y = file_parser.load(train_amount=26, test_amount=26)
 
     network = NeuralNetwork(
         [
-            Convolutional(filters_num=32, filter_size=5, channels=1),
+            Convolutional(filters_num=16, filter_size=3, channels=1),
+            Relu(),
+            Pooling(filter_size=2, stride=2),
+            Convolutional(filters_num=32, filter_size=3, channels=16),
             Relu(),
             Pooling(filter_size=2, stride=2),
             Convolutional(filters_num=64, filter_size=3, channels=32),
             Relu(),
-            Pooling(filter_size=2, stride=2),
             Flatten(),
+            Dense(576, 256),
+            Relu(),
             Dropout(0.2),
-            Dense(1600, 600),
+            Dense(256, 128),
             Relu(),
-            Dense(600, 100),
-            Relu(),
-            Dense(100, 26),
+            Dropout(0.2),
+            Dense(128, 26),
             Softmax()
         ],
 
@@ -42,7 +38,7 @@ def main():
         objects=file_parser.get_objects()
     )
 
-    network.load("NeuralNetwork/Models/temp.pkl")
+
     network.train(train_x, train_y, test_x, test_y, epochs=900)
     network.save("NeuralNetwork/Models/10_new")
 
